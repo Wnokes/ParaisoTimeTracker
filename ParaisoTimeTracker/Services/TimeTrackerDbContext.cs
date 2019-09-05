8c526e6fa401eb296838fbfc815c6ff573274714
Wnokes <westonroynokes@gmail.com>
2019-09-05 00:38:07 -0600

    commiting again cause i have nested gits?

---
+++ ParaisoTimeTracker/Services/TimeTrackerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ParaisoTimeTracker.Models;
+
+namespace ParaisoTimeTracker.Services
+{
+    public class TimeTrackerDbContext : DbContext
+    {
+        public TimeTrackerDbContext(DbContextOptions<DbContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public virtual DbSet<Company> Companies { get; set; }
+        public virtual DbSet<CompanyAdmin> CompanyAdmins { get; set; }
+        public virtual DbSet<Employee> Employees{ get; set; }
+        public virtual DbSet<ParaisoAdmin> ParaisoAdmins{ get; set; }
+        public virtual DbSet<TimeEntry> TimeEntries{ get; set; }
+        public virtual DbSet<User> Users{ get; set; }
+    }
+}
--- ParaisoTimeTracker/Startup.cs
+++ ParaisoTimeTracker/Startup.cs
@@ -14,10 +14,20 @@ namespace ParaisoTimeTracker
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            var connectionString = Configuration["connectionString:timeTrackerDbConnectionString"];
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
